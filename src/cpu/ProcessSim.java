a920425d58cbc5be1229c1c7c4304be012f30b27
jan0018 <jan0018@uah.edu>
2021-10-22 16:49:13 -0500

    Added comments to ProcessSim

--- src/cpu/ProcessSim.java
+++ src/cpu/ProcessSim.java
@@ -15,46 +15,93 @@ public class ProcessSim {
 	private long tat = 0;
 	private double ntat = 0;
     
+	/**
+	 * Gets normalized turnaround time.
+	 * @return normalized turnaround time
+	 */
 	public double getNtat() {
 		return ntat;
 	}
 	
+	/**
+	 * Sets normalized turnaround time.
+	 * @param new normalized turnaround time
+	 */
 	public void setNtat(double ntat) {
 		this.ntat = ntat;
 	}
 
+	/**
+	 * Gets turnaround time.
+	 * @return turnaround time
+	 */
 	public long getTat() {
 		return tat;
 	}
 	
+	/**
+	 * Sets turnaround time.
+	 * @param new turnaround time
+	 */
 	public void setTat(long tat) {
 		this.tat = tat;
 	}
 	
+	/**
+	 * Gets the actual service time.
+	 * @return actual service time
+	 */
     public long getActualServiceTime() {
 		return actualServiceTime;
 	}
 
+    /** 
+     * Sets the actual service time.
+     * @param new actual service time
+     */
 	public void setActualServiceTime(long actualServiceTime) {
 		this.actualServiceTime = actualServiceTime;
 	}
 	
+	/**
+	 * Gets the actual finish time.
+	 * @return actual finish time
+	 */
     public long getActualFinishTime() {
 		return actualFinishTime;
 	}
 
+    /**
+     * Sets the actual finish time.
+     * @param new actual finish time
+     */
 	public void setActualFinishTime(long actualFinishTime) {
 		this.actualFinishTime = actualFinishTime;
 	}
 	
+	/**
+	 * Gets the actual arrival time.
+	 * @return actual arrival time
+	 */
     public long getActualArrivalTime() {
 		return actualArrivalTime;
 	}
 
+    /** 
+     * Sets the actual arrival time.
+     * @param new actual arrival time
+     */
 	public void setActualArrivalTime(long actualArrivalTime) {
 		this.actualArrivalTime = actualArrivalTime;
 	}
 
+	/**
+	 * Constructor for ProcessSim.
+	 * @param arrival time
+	 * @param process name
+	 * @param service time
+	 * @param priority
+	 */
 	public ProcessSim (long arrivalTime, String processName, long serviceTime, int priority)
     {
         this.arrivalTime = arrivalTime;
@@ -63,22 +110,42 @@ public class ProcessSim {
         this.priority = priority;
     }
 
+	/**
+	 * Sets service time.
+	 * @param new service time
+	 */
     public void setServiceTime(long serviceTime) {
         this.serviceTime = serviceTime;
     }
 
+    /**
+     * Gets arrival time.
+     * @return arrival time
+     */
     public long getArrivalTime() {
         return arrivalTime;
     }
-
+    
+    /**
+     * Gets service time.
+     * @return service time
+     */
     public long getServiceTime() {
         return serviceTime;
     }
 
+    /**
+     * Gets process name.
+     * @return process name
+     */
     public String getProcessName() {
         return processName;
     }
 
+    /**
+     * Gets priority.
+     * @return priority
+     */
     public int getPriority() {
         return priority;
     }
